5cab056a6d6e81746d71db3d67bd87b5ae92ff36
ByeongHunKim <rkwhr912@naver.com>
2022-10-15 16:52:43 +0900

    #6 add : 지갑연결, 컨트랙트 연결 함수 추가

--- web/mokkoji/src/components/HelloWorld.vue
+++ web/mokkoji/src/components/HelloWorld.vue
@@ -3,15 +3,24 @@
     <!-- vue에서 if문 사용 : 메타마스크가 설치되었으면 아래 문구 렌더링, 미설치 시 v-else 가 적용되어있는 div태그 렌더링 -->
     <!-- v-if, else는 꼭 붙어있어야 한다. -->
     <div v-if="isInstallMetaMask">
-      메타마스크 설치됨
+      <div>메타마스크 설치됨</div>
+      <div v-if="myAddress == null">
+        <button v-on:click="connectWallet">메타마스크 지갑 연결</button>
+      </div>
+      <div v-else>
+        <button>컨트랙트 연결</button>
+      </div>
     </div>
     <div v-else>
-      메타마스크 설치되지 않음
+      <div>메타마스크 설치되지 않음</div>
     </div>
   </div>
 </template>
 
 <script>
+import {ethers} from "ethers";
+import abi from "../assets/abi.json";
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -20,11 +29,27 @@ export default {
   data(){
     return{
       // 이 변수에는 뒤의 조건이 모두 true일 때 true가 저장된다.
-      isInstallMetaMask : window.ethereum.isMetaMask != null && window.ethereum.isMetaMask == true
+      isInstallMetaMask : window.ethereum.isMetaMask != null && window.ethereum.isMetaMask == true,
+      myAddress : null,
     }
   },
   methods:{
-
+    async connectWallet()
+    { 
+      console.log("click : connectWallet");
+      var addressArray = await window.ethereum.request({method : "eth_requestAccounts"});
+      // 연결된 지갑의 주소값이 배열에 들어있다.
+      if ( addressArray.length > 0)
+      {
+        this.myAddress = addressArray[0];
+      }
+      console.log(addressArray);
+    },
+    connectToContract()
+    {
+      console.log("click : connect to contract");
+      var contractAddress = "0x60aCf6de68fEd0c2608e9DC96077f51786c13dda";
+    }
   }
 }
 </script>
