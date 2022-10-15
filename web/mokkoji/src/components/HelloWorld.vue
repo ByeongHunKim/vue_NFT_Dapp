bccc0b42d805374bbf8a58e8aaacc5047a4cd593
ByeongHunKim <rkwhr912@naver.com>
2022-10-15 15:57:44 +0900

    #5 add :  metamask 설치여부

--- web/mokkoji/src/components/HelloWorld.vue
+++ web/mokkoji/src/components/HelloWorld.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <!-- vue에서 if문 사용 : 메타마스크가 설치되었으면 아래 문구 렌더링, 미설치 시 v-else 가 적용되어있는 div태그 렌더링 -->
+    <!-- v-if, else는 꼭 붙어있어야 한다. -->
+    <div v-if="isInstallMetaMask">
+      메타마스크 설치됨
+    </div>
+    <div v-else>
+      메타마스크 설치되지 않음
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +16,15 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      // 이 변수에는 뒤의 조건이 모두 true일 때 true가 저장된다.
+      isInstallMetaMask : window.ethereum.isMetaMask != null && window.ethereum.isMetaMask == true
+    }
+  },
+  methods:{
+
   }
 }
 </script>
