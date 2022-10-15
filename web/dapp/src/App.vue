ab0513315c2a6393f27dd68d19ec8b5aef2c4bc5
ByeongHunKim <rkwhr912@naver.com>
2022-10-15 15:06:14 +0900

    #2 edit test

--- web/dapp/src/App.vue
+++ web/dapp/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="창작모꼬지 2기" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
